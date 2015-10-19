2de8dac049d4691bfb48e22d80a0d5739efdda53
igor <shevchenkcoigor@gmail.com>
2015-10-19 18:46:33 +0300

    add tornado server

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,5 @@
 __author__ = 'igor'
 
-
-__author__ = 'igor'
-
 from fabric.api import local, run, cd, settings
 import os
 from clavutich.settings import BASE_DIR
@@ -44,15 +41,14 @@ def local_act():
     prepare deploy
     :return: None
     """
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neatapps.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clavutich.settings")
     activate_env = os.path.expanduser(os.path.join(BASE_DIR, ".env/bin/activate_this.py"))
     execfile(activate_env, dict(__file__=activate_env))
     local("./manage.py test")
-    local("./manage.py compilemessages")
     local("./manage.py makemigrations")
     local("./manage.py migrate")
     local("%s%s" % ('pip freeze > ', REQUIREMENTS_FILE))
-    local("./manage.py collectstatic --noinput -c")
+    local("./manage.py collectstatic --noinput")
     local("git add .")
     local("git commit -a -F git_commit_message")
     current_branch = local("git symbolic-ref --short -q HEAD", capture=True)
@@ -62,10 +58,8 @@ def local_act():
         local("git merge %s" % current_branch)
         local("git branch -d %s" % current_branch)
 
-    local("git push origin")
+    local("git push bit")
     local("git push production")
-    local("git push my_repo_neatapps_bit")
-    local("git push my-production")
 
 
 def update_requirements():

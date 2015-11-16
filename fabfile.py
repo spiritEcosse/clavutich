4b784ed1c5c4a03502ac3e14b739390705129631
igor <shevchenkcoigor@gmail.com>
2015-11-16 17:41:45 +0200

    fix bag with csrf token

--- fabfile.py
+++ fabfile.py
@@ -37,7 +37,7 @@ def remote_act():
 
                 with prefix('source .env/bin/activate'):
                     run("./manage.py migrate")
-                    # run("./manage.py loaddata db.json")
+                    run("./manage.py loaddata db.json")
 
                 pids = run("ps -ef|grep -v grep |grep '%s' | awk '{print $2}'" % PROJECT_NAME)
 
@@ -65,7 +65,7 @@ def local_act():
     local("grunt default")
     local("./manage.py makemigrations")
     local("./manage.py migrate")
-    # local("./manage.py dumpdata --indent 4 --natural -e contenttypes -e auth.Permission -e sessions -e admin > db.json")
+    local("./manage.py dumpdata --indent 4 --natural -e contenttypes -e auth.Permission -e sessions -e admin > db.json")
     local('pip freeze > ' + REQUIREMENTS_FILE)
     local("./manage.py collectstatic --noinput -c")
     local("git add .")

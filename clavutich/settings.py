e6f3b8c9af84d00afb8fd7706f805cdf0c74edbf
igor <shevchenkcoigor@gmail.com>
2015-11-18 12:26:28 +0200

    add GZipMiddleware

--- clavutich/settings.py
+++ clavutich/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
